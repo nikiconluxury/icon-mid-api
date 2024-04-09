f9486eff42b3078a48e070aaccf22cb137f248cc
nik popov <niK@iconluxurygroup.com>
2024-04-09 21:56:08 +0000

    rm writing none found as url

--- main.py
+++ main.py
@@ -316,11 +316,12 @@ def write_failed_downloads_to_excel(failed_downloads, excel_file):
             url = row[0]
             row_id = row[1]
             if url:
-                # Write the URL to column A of the failed row
-                # Adjust the cell reference as needed (row index might need +1 depending on header row)
-                cell_reference = f"{get_column_letter(1)}{row_id}"  # Column A, row number
-                worksheet[cell_reference] = str(url)
-                highlight_cell(excel_file, cell_reference)
+                if url != 'None found in this filter':
+                    # Write the URL to column A of the failed row
+                    # Adjust the cell reference as needed (row index might need +1 depending on header row)
+                    cell_reference = f"{get_column_letter(1)}{row_id}"  # Column A, row number
+                    worksheet[cell_reference] = str(url)
+                    highlight_cell(excel_file, cell_reference)
         workbook.save(excel_file)
         logger.info(f"Failed downloads written to Excel file: {excel_file}")
     else:
